060c6e5108cb0b388333195ace561f8f999a357d
William Zhang <WilliamHYZhang@gmail.com>
2021-02-13 18:28:41 -0500

    Create builtInMemes.js

---
+++ builtInMemes.js
@@ -0,0 +1,12 @@
+module.exports = {
+  '/spongebob': 102156234,
+  '/changemymind': 129242436,
+  '/twobuttons': 87743020,
+  '/hotlinebling': 181913649,
+  '/onedoesnotsimply': 61579,
+  '/xxeverywhere': 91538330,
+  '/cheers': 5496396,
+  '/doge': 8072285,
+  '/grusplan': 131940431,
+  '/headout': 196652226,
+}
